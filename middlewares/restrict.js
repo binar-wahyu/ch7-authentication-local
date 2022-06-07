411dc1285f60ebe692958e0b8b65552b26fa0e1e
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-06-07 22:22:22 +0800

    Remove console.log on middleware

--- middlewares/restrict.js
+++ middlewares/restrict.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
   // Bila request berasal dari user yang terautentikasi,
   // maka kita akan lanjut menjalankan handler berikutnya
-  console.log(req.isAuthenticated());
   if (req.isAuthenticated()) return next();
   // Bila tidak, kita akan redirect ke halaman login
   res.redirect("/login");
